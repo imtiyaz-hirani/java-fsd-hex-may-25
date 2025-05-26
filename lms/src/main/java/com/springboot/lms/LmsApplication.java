3217c5819847ba5ed5d5c540b7468895da336a55
Wiz-TechSkillsIT <wiztechskillsit@gmail.com>
2025-05-26 10:40:48 +0530

    lms-initial-setup

---
+++ lms/src/main/java/com/springboot/lms/LmsApplication.java
@@ -0,0 +1,22 @@
+package com.springboot.lms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+public class LmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LmsApplication.class, args);
+	}
+
+}
+/*
+ * (exclude = SecurityAutoConfiguration.class)
+ * I am telling spring to exclude security dependency for now as i haven't c
+ * configured it. 
+ * i will activate it later on. 
+ * 
+ * ctrl + shft + O : auto imports 
+ */
