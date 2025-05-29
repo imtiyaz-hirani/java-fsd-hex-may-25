1b246ef68c13b09e88d2cd16f45dcdbc4c6a8cc7
Wiz-TechSkillsIT <wiztechskillsit@gmail.com>
2025-05-29 14:22:31 +0530

    complete-security-pointing-to-db

--- lms/src/main/java/com/springboot/lms/SecurityConfig.java
+++ lms/src/main/java/com/springboot/lms/SecurityConfig.java
@@ -2,33 +2,16 @@ package com.springboot.lms;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration //<- This ensures that this class gets called during every API call
 public class SecurityConfig {
-	
-	@Bean
-	UserDetailsService users() {  	//<- In memory Authentication
-		UserDetails user = User.builder()
-			.username("user")
-			.password("{noop}user123")   //<- noop says spring, i am going to use plain text password for now
-			.roles("USER")
-			.build();
-		UserDetails admin = User.builder()
-			.username("admin")
-			.password("{noop}admin123")
-			.roles("ADMIN")
-			.build();
-		return new InMemoryUserDetailsManager(user, admin);
-	}
 	
 	@Bean
 	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -46,4 +29,10 @@ public class SecurityConfig {
 	PasswordEncoder passwordEncoder() {  //<- Bean saves this object in spring's context
 		return new BCryptPasswordEncoder();
 	}
+	
+	@Bean
+	AuthenticationManager getAuthManager(AuthenticationConfiguration auth) 
+			throws Exception {
+		  return auth.getAuthenticationManager();
+	 }
 }
