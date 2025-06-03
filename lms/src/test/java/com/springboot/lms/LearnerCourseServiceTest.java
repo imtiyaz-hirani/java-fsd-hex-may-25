53ed75138a7811d4a46690d3ae2ca015890f40c7
Wiz-TechSkillsIT <wiztechskillsit@gmail.com>
2025-06-03 15:19:40 +0530

    enroll testcase with mocking

--- lms/src/test/java/com/springboot/lms/LearnerCourseServiceTest.java
+++ lms/src/test/java/com/springboot/lms/LearnerCourseServiceTest.java
@@ -1,18 +1,24 @@
 package com.springboot.lms;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.springboot.lms.exception.ResourceNotFoundException;
 import com.springboot.lms.model.Course;
 import com.springboot.lms.model.Learner;
+import com.springboot.lms.model.LearnerCourse;
+import com.springboot.lms.repository.CourseRepository;
 import com.springboot.lms.repository.LearnerCourseRepository;
 import com.springboot.lms.repository.LearnerRepository;
 import com.springboot.lms.service.LearnerCourseService;
@@ -26,6 +32,32 @@ public class LearnerCourseServiceTest {
     private LearnerRepository learnerRepository;
     @Mock
     private LearnerCourseRepository learnerCourseRepository;
+    @Mock
+    private CourseRepository courseRepository;
+
+    private Learner learner;
+    private Course course;
+    private LearnerCourse learnerCourse;
+
+    @BeforeEach
+    public void init() {
+        learner = new Learner();
+        learner.setId(1);
+        learner.setName("Harry Potter");
+        learner.setContact("98667434344");
+
+        course = new Course();
+        course.setId(1);
+        course.setTitle("my course");
+        course.setCredits(1000);
+
+        learnerCourse = new LearnerCourse();
+        learnerCourse.setId(25);
+        learnerCourse.setLearner(learner);
+        learnerCourse.setCourse(course);
+        learnerCourse.setCouponCode("ABC");
+        learnerCourse.setEnrollDate(LocalDate.now());
+    }
 
     // @Test
     public void getCoursesByLearnerIdTest() {
@@ -46,15 +78,6 @@ public class LearnerCourseServiceTest {
     @Test
     public void getCoursesByLearnerIdTestMock() {
         /* Prepare mocked excepted result */
-        Learner learner = new Learner();
-        learner.setId(1);
-        learner.setName("Harry Potter");
-        learner.setContact("98667434344");
-
-        Course course = new Course();
-        course.setId(1);
-        course.setTitle("my course");
-        course.setCredits(1000);
         List<Course> expectedList = List.of(course);
 
         when(learnerRepository.findById(1)).thenReturn(Optional.of(learner));
@@ -65,5 +88,28 @@ public class LearnerCourseServiceTest {
         List<Course> actualList = learnerCourseService.getCoursesByLearnerId(1);
 
         assertEquals(expectedList, actualList);
+    }
+
+    @Test
+    public void enrollLearnerInCourse() {
+        /** Expectation */
+        LearnerCourse lc = new LearnerCourse();
+        when(learnerRepository.findById(100)).thenReturn(Optional.of(learner));
+        when(courseRepository.findById(50)).thenReturn(Optional.of(course));
+        when(learnerCourseRepository.save(lc)).thenReturn(learnerCourse);
+
+        /** Actual */
+        assertEquals(learnerCourse,
+                learnerCourseService.enrollLearnerInCourse(100, 50, lc));
+
+        // use case for invalid learner ID
+        ResourceNotFoundException e = assertThrows(ResourceNotFoundException.class,
+                () -> learnerCourseService.enrollLearnerInCourse(99, 50, lc));
+        assertEquals("Learner ID Invalid".toLowerCase(), e.getMessage().toLowerCase());
+
+        // use case for invalid course ID
+        e = assertThrows(ResourceNotFoundException.class,
+                () -> learnerCourseService.enrollLearnerInCourse(100, 49, lc));
+        assertEquals("Course ID Invalid".toLowerCase(), e.getMessage().toLowerCase());
     }
 }
