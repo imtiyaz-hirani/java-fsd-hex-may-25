644a10fde17a00d4a24160b052e92c09f3866c7c
Wiz-TechSkillsIT <wiztechskillsit@gmail.com>
2025-06-11 11:56:16 +0530

    axios added for API calls

--- React UI/react-lms-ui/src/components/Post.jsx
+++ React UI/react-lms-ui/src/components/Post.jsx
@@ -1,53 +1,60 @@
+import axios from "axios";
 import { useEffect, useState } from "react";
 
 function Post() {
     let [postArry, setPostArry] = useState([]);
     useEffect(() => {
-        //Call the API : fetch 
-        fetch('https://jsonplaceholder.typicode.com/posts')
-            .then(resp => resp.json())
-            .then(data => setPostArry(data))
+        const getPosts = async () => {
+            try {
+                const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
+                //console.log(response) <-- I get the response structure from here... 
+                setPostArry(response.data)
+            } catch (err) {
+                console.log(err)
+            }
+        }
+        getPosts(); //<-- calling the function to ensure API is called.. 
     }, []);
     return (
         <div className="container-fluid">
-            <nav class="navbar navbar-expand-lg navbar-light bg-light mb-4">
-                <div class="container-fluid">
-                    <a class="navbar-brand" href="#">Navbar</a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light mb-4">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#">Navbar</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div class="collapse navbar-collapse" id="navbarNav">
-                        <ul class="navbar-nav">
-                            <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    <div className="collapse navbar-collapse" id="navbarNav">
+                        <ul className="navbar-nav">
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="#">Home</a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Features</a>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Features</a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Pricing</a>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Pricing</a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                            <li className="nav-item">
+                                <a className="nav-link disabled" href="#" >Disabled</a>
                             </li>
                         </ul>
                     </div>
                 </div>
             </nav>
-            <div><h3 class="display-5">All Posts</h3></div>
+            <div><h3 className="display-5">All Posts</h3></div>
             <div className="row">
 
                 {
                     postArry.map((post) => (
-                        <div className="col-md-12 mb-2">
-                            <div class="card" key={post.id}>
-                                <div class="card-header">
+                        <div className="col-md-12 mb-2" key={post.id}>
+                            <div className="card" >
+                                <div className="card-header">
                                     User ID: {post.userId}
                                 </div>
-                                <div class="card-body">
-                                    <h5 class="card-title"> {post.title}</h5>
-                                    <p class="card-text">{post.body}</p>
-                                    <a href="#" class="btn btn-primary">View Comments</a>
+                                <div className="card-body">
+                                    <h5 className="card-title"> {post.title}</h5>
+                                    <p className="card-text">{post.body}</p>
+                                    <a href="#" className="btn btn-primary">View Comments</a>
                                 </div>
                             </div>
                         </div>
